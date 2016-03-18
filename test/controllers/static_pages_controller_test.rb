2c792e367fe6fa5136ea6e4097fc63b0ec667708
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-03-18 18:37:02 -0400

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Epic Sample App"
+    assert_select "title", "Epic Sample App"
   end
 
   test "should get help" do
@@ -17,6 +17,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | Epic Sample App"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Epic Sample App"
   end
 
 end
